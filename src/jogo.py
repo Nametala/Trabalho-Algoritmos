b3ee5579600d5b25e3ddcd8c9d84ca04d4714696
Nametala <arthurnametalla@gmail.com>
2026-06-10 22:11:07 -0300

    Implementação hud pontuação

--- src/jogo.py
+++ src/jogo.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-import sys # Importado para garantir a saída limpa do sistema
+import sys
 from src.sprites import Projetil
 
 from src.config import (
@@ -65,7 +65,7 @@ def executar_jogo():
         mostrar_tela_inicial(tela, fundo_inicial)
 
         # CONFIGURAÇÕES DA PARTIDA (Resetadas toda vez que o jogo recomeça)
-        VIDA_INIMIGO_PADRAO = 2  
+        VIDA_INIMIGO_PADRAO = 3  
         DANO_TIRO = 1            
         
         velocidade_inimigo = 2   
@@ -83,143 +83,171 @@ def executar_jogo():
         player_image.set_colorkey((0, 0, 0))
         player_image = player_image.convert_alpha()
 
-        imagem_barra_vida_original = pygame.image.load(CAMINHO_VIDA_CHEIA).convert_alpha()
-        escala_barra_vida = 5 
-        largura_barra_vida = int(imagem_barra_vida_original.get_width() * escala_barra_vida)
-        altura_barra_vida = int(imagem_barra_vida_original.get_height() * escala_barra_vida)
-        barra_vida_image = pygame.transform.scale(imagem_barra_vida_original, (largura_barra_vida, altura_barra_vida))
-        barra_vida_image = barra_vida_image.convert_alpha()
 
 
+        # SISTEMA BARRA DE VIDA 
+        escala_barra_vida = 5
 
-        imagem_inimigo_original = pygame.image.load(CAMINHO_INIMIGO).convert_alpha()
-        escala_inimigo = 2
-        largura_inimigo = int(imagem_inimigo_original.get_width() * escala_inimigo)
-        altura_inimigo = int(imagem_inimigo_original.get_height() * escala_inimigo)
-        enemy_image = pygame.transform.scale(imagem_inimigo_original, (largura_inimigo, altura_inimigo))
-        
-        posicao_inicial_x = (LARGURA_TELA // 2) - (largura_nave // 2)
-        posicao_inicial_y = ALTURA_TELA - altura_nave - 20
+        vidas = 3   #Quantidade vidas jogador
 
-        posicao_barra_vida_x = posicao_inicial_x
-        posicao_barra_vida_y = (posicao_inicial_y - 40)
+        def barra_de_vida(caminho):
+            img = pygame.image.load(caminho).convert_alpha()
+            larg = int(img.get_width() * escala_barra_vida)
+            alt = int(img.get_height() * escala_barra_vida)
+            img_escalada = pygame.transform.scale(img, (larg, alt))
+            
+            return img_escalada
 
-        jogador = {
-            "imagem": player_image,
-            "rect": player_image.get_rect(topleft=(posicao_inicial_x, posicao_inicial_y))
+        # Variações da barra de vida
+        sprites_vida = {
+            3: barra_de_vida(CAMINHO_VIDA_CHEIA),
+            2: barra_de_vida(CAMINHO_23_VIDA),
+            1: barra_de_vida(CAMINHO_13_VIDA),
+            0: barra_de_vida(CAMINHO_VIDA_VAZIA)
         }
 
-        grupo_tiros = pygame.sprite.Group()
-        lista_inimigos = []
+        # LOOP PRINCIPAL DA SESSÃO
+        while True:
+            mostrar_tela_inicial(tela, fundo_inicial)
 
-        temporizador_spawn = 0
-        pontos = 0
-        vidas = 3   
-        recorde = carregar_recorde(CAMINHO_RECORDE)
+            imagem_inimigo_original = pygame.image.load(CAMINHO_INIMIGO).convert_alpha()
+            escala_inimigo = 2
+            largura_inimigo = int(imagem_inimigo_original.get_width() * escala_inimigo)
+            altura_inimigo = int(imagem_inimigo_original.get_height() * escala_inimigo)
+            enemy_image = pygame.transform.scale(imagem_inimigo_original, (largura_inimigo, altura_inimigo))
+            
+            posicao_inicial_x = (LARGURA_TELA // 2) - (largura_nave // 2)
+            posicao_inicial_y = ALTURA_TELA - altura_nave - 20
 
-        rodando_partida = True
+            jogador = {
+            "imagem": player_image,
+            "rect": player_image.get_rect(topleft=(posicao_inicial_x, posicao_inicial_y))                   
+            }
 
-        # LOOP DE GAMEPLAY (A partida em si)
-        while rodando_partida:
-            relogio.tick(FPS)
+            # Fixar na esquerda
+            posicao_barra_vida_x = 20  
+            posicao_barra_vida_y = 330
 
-            for evento in pygame.event.get():
-                if evento.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                
+            barra_vida = {
+                "imagem": sprites_vida[3], 
+                "rect": sprites_vida[3].get_rect(topleft=(posicao_barra_vida_x, posicao_barra_vida_y))
+            }
+
+            grupo_tiros = pygame.sprite.Group()
+            lista_inimigos = []
+
+            temporizador_spawn = 0
+            pontos = 0
+            recorde = carregar_recorde(CAMINHO_RECORDE)
+
+            rodando_partida = True
+
+            # LOOP DE GAMEPLAY 
+            while rodando_partida:
+                relogio.tick(FPS)
+
+                for evento in pygame.event.get():
+                    if evento.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
                     
+                        
 
-                # Disparar tiro
-                if evento.type == pygame.KEYDOWN and evento.key == pygame.K_SPACE:
-                    novo_tiro = Projetil(jogador["rect"].centerx, player_image.get_rect(topleft=(jogador["rect"].x, jogador["rect"].y)).top, CAMINHO_TIRO)
-                    grupo_tiros.add(novo_tiro)
-                elif evento.type == pygame.MOUSEBUTTONDOWN:
-                    novo_tiro = Projetil(jogador["rect"].centerx, player_image.get_rect(topleft=(jogador["rect"].x, jogador["rect"].y)).top, CAMINHO_TIRO)
-                    grupo_tiros.add(novo_tiro)
+                    # Disparar tiro
+                    if evento.type == pygame.KEYDOWN and evento.key == pygame.K_SPACE:
+                        novo_tiro = Projetil(jogador["rect"].centerx, player_image.get_rect(topleft=(jogador["rect"].x, jogador["rect"].y)).top, CAMINHO_TIRO)
+                        grupo_tiros.add(novo_tiro)
+                    elif evento.type == pygame.MOUSEBUTTONDOWN:
+                        novo_tiro = Projetil(jogador["rect"].centerx, player_image.get_rect(topleft=(jogador["rect"].x, jogador["rect"].y)).top, CAMINHO_TIRO)
+                        grupo_tiros.add(novo_tiro)
 
-            # Movimentação W A S D
-            teclas = pygame.key.get_pressed()
-            if teclas[pygame.K_a]:
-                jogador["rect"].x -= velocidade
-            if teclas[pygame.K_d]:
-                jogador["rect"].x += velocidade
-            if teclas[pygame.K_w]:
-                jogador["rect"].y -= velocidade
-            if teclas[pygame.K_s]:
-                jogador["rect"].y += velocidade
+                # Movimentação W A S D
+                teclas = pygame.key.get_pressed()
+                if teclas[pygame.K_a]:
+                    jogador["rect"].x -= velocidade
+                if teclas[pygame.K_d]:
+                    jogador["rect"].x += velocidade
+                if teclas[pygame.K_w]:
+                    jogador["rect"].y -= velocidade
+                if teclas[pygame.K_s]:
+                    jogador["rect"].y += velocidade
 
-            # Bordas da tela
-            if jogador["rect"].left < 0: jogador["rect"].left = 0
-            if jogador["rect"].right > LARGURA_TELA: jogador["rect"].right = LARGURA_TELA
-            if jogador["rect"].top < 0: jogador["rect"].top = 0
-            if jogador["rect"].bottom > ALTURA_TELA: jogador["rect"].bottom = ALTURA_TELA
+                # Bordas da tela
+                if jogador["rect"].left < 0: jogador["rect"].left = 0
+                if jogador["rect"].right > LARGURA_TELA: jogador["rect"].right = LARGURA_TELA
+                if jogador["rect"].top < 0: jogador["rect"].top = 0
+                if jogador["rect"].bottom > ALTURA_TELA: jogador["rect"].bottom = ALTURA_TELA
 
-            # Spawn de inimigos
-            temporizador_spawn += 1
-            if temporizador_spawn >= frequencia_spawn:
-                temporizador_spawn = 0
-                x_spawn = random.randint(0, LARGURA_TELA - largura_inimigo)
-                y_spawn = -altura_inimigo
-                novo_inimigo = {
-                    "rect": enemy_image.get_rect(topleft=(x_spawn, y_spawn)),
-                    "vida": VIDA_INIMIGO_PADRAO  
-                }
-                lista_inimigos.append(novo_inimigo)
+                # Spawn de inimigos
+                temporizador_spawn += 1
+                if temporizador_spawn >= frequencia_spawn:
+                    temporizador_spawn = 0
+                    x_spawn = random.randint(0, LARGURA_TELA - largura_inimigo)
+                    y_spawn = -altura_inimigo
+                    novo_inimigo = {
+                        "rect": enemy_image.get_rect(topleft=(x_spawn, y_spawn)),
+                        "vida": VIDA_INIMIGO_PADRAO  
+                    }
+                    lista_inimigos.append(novo_inimigo)
 
-            # Movimento dos inimigos
-            for ini in lista_inimigos[:]:
-                ini["rect"].y += velocidade_inimigo
-                if ini["rect"].top > ALTURA_TELA:
-                    vidas = tomar_dano(vidas, 100) # Inimigo passou causa game over imediato
-                    lista_inimigos.remove(ini)
-
-            grupo_tiros.update() 
-
-            # Colisão: Projéteis vs Inimigos
-            for tiro in grupo_tiros.sprites():
+                # Movimento dos inimigos
                 for ini in lista_inimigos[:]:
-                    if verificar_colisao(tiro.rect, ini["rect"]):
-                        tiro.kill()
-                        ini["vida"] -= DANO_TIRO
-                        if ini["vida"] <= 0:
-                            lista_inimigos.remove(ini)  
-                            pontos = calcular_pontos(pontos, 10)
-                        break
+                    ini["rect"].y += velocidade_inimigo
+                    if ini["rect"].top > ALTURA_TELA:
+                        vidas = tomar_dano(vidas, 100) # Inimigo passou causa game over imediato
+                        lista_inimigos.remove(ini)
 
-            # Colisão: Inimigos vs Jogador
-            for ini in lista_inimigos[:]:
-                if verificar_colisao(jogador["rect"], ini["rect"]):
-                    vidas = tomar_dano(vidas, 1)
-                    lista_inimigos.remove(ini)  
+                grupo_tiros.update() 
 
-            # CORREÇÃO: Condições de Fim de Jogo 
-            if jogador_perdeu(vidas):
-                rodando_partida = False # Quebra o loop da gameplay e vai para a Tela Final
+                # Colisão: Projéteis vs Inimigos
+                for tiro in grupo_tiros.sprites():
+                    for ini in lista_inimigos[:]:
+                        if verificar_colisao(tiro.rect, ini["rect"]):
+                            tiro.kill()
+                            ini["vida"] -= DANO_TIRO
+                            if ini["vida"] <= 0:
+                                lista_inimigos.remove(ini)  
+                                pontos = calcular_pontos(pontos, 10)
+                            break
 
-            if pontos > recorde:
-                recorde = pontos
-                salvar_recorde(CAMINHO_RECORDE, recorde)
+                # Colisão: Inimigos vs Jogador
+                for ini in lista_inimigos[:]:
+                    if verificar_colisao(jogador["rect"], ini["rect"]):
+                        vidas = tomar_dano(vidas, 1)
+                        lista_inimigos.remove(ini)  
 
-            pygame.display.set_caption(
-                f"{TITULO_JOGO} | Pontos: {pontos} | Recorde: {recorde} | Vidas: {vidas}"
-            )
+                vidas_checadas = max(0, min(vidas, 3))
+                barra_vida["imagem"] = sprites_vida[vidas_checadas]
 
-            # Renderização
-            tela.blit(imagem_fundo, (0, 0)) 
-            for ini in lista_inimigos:
-                tela.blit(enemy_image, ini["rect"])
-            tela.blit(jogador["imagem"], jogador["rect"])
-            grupo_tiros.draw(tela) 
-            mostrar_pontos(tela,pontos,recorde)
-            pygame.display.flip()
+                # CORREÇÃO: Condições de Fim de Jogo 
+                if jogador_perdeu(vidas):
+                    rodando_partida = False # Quebra o loop da gameplay e vai para a Tela Final
+                    vidas = 3
 
-        # Se saiu do loop 'rodando_partida', significa que o jogador morreu!
-        # Chamamos a tela final antes de reiniciar o loop principal.
-        mostrar_pontos(tela,pontos,recorde)
-        mostrar_tela_final(tela, fundo_final)
-        
+                if pontos > recorde:
+                    recorde = pontos
+                    salvar_recorde(CAMINHO_RECORDE, recorde)
 
+                pygame.display.set_caption(
+                    f"{TITULO_JOGO} | Pontos: {pontos} | Recorde: {recorde} | Vidas: {vidas}"
+                )
+
+                # Renderização
+
+                tela.blit(imagem_fundo, (0, 0)) 
+                for ini in lista_inimigos:
+                    tela.blit(enemy_image, ini["rect"])
+                tela.blit(jogador["imagem"], jogador["rect"])
+                grupo_tiros.draw(tela) 
+
+                tela.blit(barra_vida["imagem"], barra_vida["rect"])
+
+                mostrar_pontos(tela,pontos, recorde)
+
+                pygame.display.flip()
+
+            # Chamamos a tela final antes de reiniciar o loop principal.
+            mostrar_tela_final(tela, fundo_final)
 
 def mostrar_tela_inicial(tela, fundo_inicial):
     fonte_titulo = pygame.font.Font(None, 74)
@@ -270,18 +298,11 @@ def mostrar_tela_final(tela, fundo_final):
         tela.blit(texto_subtitulo, retangulo_subtitulo)
         pygame.display.flip()
 
-def mostrar_pontos(tela,pontos, recorde):
-    fonte_pontos = pygame.font.Font(None, 74)
-    texto_pontos = fonte_pontos.render(f"Força: {pontos}", True, BRANCO)
-
-    fonte_recorde = pygame.font.Font(None, 74)
-    texto_recorde = fonte_recorde.render(f"Record: {recorde}", True, BRANCO)
-
+def mostrar_pontos(tela, pontos, recorde):
+    fonte_hud = pygame.font.Font(None, 36)
     
-    tela.blit(texto_pontos, (10,10))
-    tela.blit(texto_recorde,(10,40))
+    texto_pontos = fonte_hud.render(f"Pontos: {pontos}", True, BRANCO)
+    texto_recorde = fonte_hud.render(f"Recorde: {recorde}", True, BRANCO)
 
-
-
-
-
+    tela.blit(texto_pontos, (20, 20))
+    tela.blit(texto_recorde, (20, 55))
